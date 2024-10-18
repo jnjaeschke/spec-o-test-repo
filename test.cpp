318c14b8529ed7eff380fbaf473158ed377e7775
Jan-Niklas Jaeschke <jjaschke@mozilla.com>
2024-10-18 14:30:39 +0200

    Added spec comments to the code.

--- test.cpp
+++ test.cpp
@@ -1,5 +1,11 @@
 #include <string>
 
+/**
+ * @brief foo function.
+ *
+ * This function follows https://example.spec/#foo
+ */
 void foo() {
     std::string bar = "hello world";
+    // bar is defined in http://example.spec/#foo-bar
 }
